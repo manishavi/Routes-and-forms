2d324aaf7ba29aaaf32065c961ed0c77a600ef92
manishavi <manishalal972@gmail.com>
2018-05-22 17:53:36 -0500

    added routes for hello and goodbye

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import './index.css';
 import App from './components/app';
 import reducers from './reducers';
@@ -9,9 +11,26 @@ import registerServiceWorker from './registerServiceWorker';
 
 const createStoreMiddleware = applyMiddleware()(createStore);
 
+class Hello extends Component {
+  render () {
+    return <div>Hello!</div>
+  }
+}
+
+class Goodbye extends Component {
+  render () {
+    return <div>Goodbye!</div>
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
