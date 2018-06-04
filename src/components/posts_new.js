3479481338077c8902d951469c438eeea9f15d56
manishavi <manishalal972@gmail.com>
2018-06-04 18:17:44 -0500

    added validate function to validate form and show error to user

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,6 +11,7 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
@@ -24,8 +25,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -37,6 +38,26 @@ class PostsNew extends Component {
     );
   }
 }
+
+function validate(values) {
+  const errors = {};
+  //validate the i/p from values
+  if (!values.title) {
+    errors.title ="Enter a title";
+  }
+
+  if (!values.categories) {
+    errors.categories ="Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content ="Enter some content";
+  }
+  //If error is empty, the form is fine to submit
+  //If error has *any* properties, redux form assumes form is invalid
+  return errors;
+}
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'//make it unique
 }) (PostsNew);
