c45a21a1096c223bac613307d601dc1c9a00b998
manishavi <manishalal972@gmail.com>
2018-06-04 11:40:05 -0500

    added navigation with link component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
 
@@ -23,6 +24,11 @@ class PostsIndex extends Component {
     console.log(this.props.posts);
     return(
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul>
           {this.renderPosts()}
