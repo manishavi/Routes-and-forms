0e2b22d670334049f52da6b88d4aec38175bb385
manishavi <manishalal972@gmail.com>
2018-06-04 16:18:53 -0500

    added field for title and tag

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
 
-export default class PostsNew extends Component {
+class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div className="form-group">
+        <label>Title</label>
+        <input 
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
+  renderTagsField(field) {
+    return (
+      <div className="form-group">
+        <label>Tags</label>
+        <input 
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return(
-      <div>
-        PostsNew
-      </div>
+      <form>
+        <Field 
+          name="title"
+          component={this.renderTitleField}
+        />
+        <Field 
+          name="tags"
+          component={this.renderTagsField}
+        />
+      </form>
     );
   }
 }
+export default reduxForm({
+  form: 'PostsNewForm'//make it unique
+}) (PostsNew);
