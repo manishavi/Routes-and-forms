c63780f09796405d7ea060dca335406a7b4667f9
manishavi <manishalal972@gmail.com>
2018-06-05 18:15:27 -0500

    added loading

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,16 +9,22 @@ class PostsShow extends Component {
   }
 
   render() {
-    //posts[this.props.match.params.id]//the post we want to show
-    return(
+    const { post } = this.props;
+
+    if(!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
       <div>
-        Posts Show
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   };
 }
 function  mapStateToProps({ posts }, ownProps) {
-  //return { posts };
   return { post: posts[ownProps.match.params.id] };
 }
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
