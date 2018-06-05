9cfd6d830f0a833790acb9fa1d9ac8845678ff57
manishavi <manishalal972@gmail.com>
2018-06-05 17:04:49 -0500

    added mapStateToProps and ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
-export default class PostsShow extends Component {
+class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    //posts[this.props.match.params.id]//the post we want to show
     return(
       <div>
         Posts Show
@@ -9,3 +17,8 @@ export default class PostsShow extends Component {
     );
   };
 }
+function  mapStateToProps({ posts }, ownProps) {
+  //return { posts };
+  return { post: posts[ownProps.match.params.id] };
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
