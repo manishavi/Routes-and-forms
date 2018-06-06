2bb913e1b075e8d2961bd6fb9613ae14c48a1354
manishavi <manishalal972@gmail.com>
2018-06-06 12:36:50 -0500

    added onDeleteClick handler

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 import { Link } from'react-router-dom';
 
 class PostsShow extends Component {
@@ -9,6 +9,13 @@ class PostsShow extends Component {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
     //}
+  }
+
+  onDeleteClick = () =>{
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -23,6 +30,12 @@ class PostsShow extends Component {
         <Link className="btn btn-primary" to="/">
           Back To Index
         </Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick ={this.onDeleteClick}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -33,4 +46,4 @@ class PostsShow extends Component {
 function  mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
